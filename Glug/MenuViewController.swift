9c80874ff0f8d117e171c9b2c66de21ccc88b8f2
Александр <peresypkin10@gmail.com>
2015-08-07 00:00:20 +0300

    create controllers

---
+++ Glug/MenuViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MenuViewController.swift
+//  Glug
+//
+//  Created by Александр on 06.08.15.
+//  Copyright (c) 2015 iOSDevCourse. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    deinit {
+        println("MenuViewController deinit")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func unwindToMainMenu(sender: UIStoryboardSegue) {
+        
+    }
+
+}
